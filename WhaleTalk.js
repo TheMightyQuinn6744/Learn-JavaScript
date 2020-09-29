626cfe658bf1308955d6f357e625b162ec6c94be
Quinn Rodney <followthewhiterabit93@gmail.com>
2020-09-29 07:41:18 -0500

    Create WhaleTalk.js
    Take a phrase and convert it into whal talk using the vowels array.

---
+++ WhaleTalk.js
@@ -0,0 +1,11 @@
+//the text we want to translate 
+let input = "Welcome to the jungle!";
+//set array equal to variable vowels, no update
+const vowels = ['a','e','i','o','u'];
+//create empty array
+let resultArray = [];
+//create for loop to iterate through input 
+for (let inputIndex = 0;input.length;inputIndex++){
+  console.log("InputIndex:  " + inputIndex[inputIndex]);
+};
+
